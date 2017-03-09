7bc20e1981d90ec68004601fc234efe6e4257247
laszlo-milu <hockeylaci@gmail.com>
2017-03-09 10:35:28 +0100

    LSD-89 unit test are finally working

--- app/src/test/java/com/codecool/actimate/ControllerTest.java
+++ app/src/test/java/com/codecool/actimate/ControllerTest.java
@@ -4,11 +4,17 @@ import com.codecool.actimate.controller.APIController;
 
 import org.json.JSONObject;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ControllerTest {
 
     @Test
@@ -16,15 +22,28 @@ public class ControllerTest {
         HashMap<String, String> data = new HashMap<String, String>();
         data.put("email", "milu_laci@codecool.com");
         data.put("password", "Codec00l");
-        System.out.println(data.toString()+ " HashMap");
+        JSONObject json = APIController.createJson(data);
 
-        JSONObject json = new JSONObject();
-        System.out.println(json);
-//        "\"email\": \"milu_laci@codecool.com\", \"password\": \"Codec00l\""
-        json.put("email", "milu_laci@codecool.com");
-        json.put("password", "Codec00l");
-        System.out.println(json);
+        assertTrue(json.get("email").equals("milu_laci@codecool.com") &&
+                    json.get("password").equals("Codec00l"));
 
-        assertEquals("\"email\": \"milu_laci@codecool.com\", \"password\": \"Codec00l\"", APIController.createJson(data));
+    }
+
+    @Test
+    public void createManyElementJsonTest() throws Exception {
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("fname", "Laci");
+        data.put("lname", "Milu");
+        data.put("age", "21");
+        data.put("gender", "male");
+        data.put("nationality", "hungarian");
+        JSONObject json = APIController.createJson(data);
+
+        assertTrue(json.get("fname").equals("Laci") &&
+                    json.get("lname").equals("Milu") &&
+                    json.get("age").equals("21") &&
+                    json.get("gender").equals("male") &&
+                    json.get("nationality").equals("hungarian"));
+
     }
 }
